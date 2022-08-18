e1c7bf766853ef64287316155f7963f9816c4a0a
Dario Fulci <DarioFulci@gmail.com>
2022-08-18 21:30:16 +0200

    initial commit

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,63 @@
+package com.amigoscode.customer;
+
+import com.amigoscode.amqp.RabbitMQMessageProducer;
+import com.amigoscode.clients.fraud.FraudClient;
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.notification.NotificationClient;
+import com.amigoscode.clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@AllArgsConstructor
+public class  CustomerService {
+
+        private final CustomerRepository customerRepository;
+        private final FraudClient fraudClient;
+
+        private final RabbitMQMessageProducer rabbitMQMessageProducer;
+
+        public void registerCustomer(CustomerRegistrationRequest request) {
+            Customer customer = Customer.builder()
+                    .firstName(request.firstName())
+                    .lastName(request.lastName())
+                    .email(request.email())
+                    .build();
+            // todo: check if email valid
+            // todo: check if email not taken
+
+            customerRepository.saveAndFlush(customer);
+
+            FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
+
+            if (fraudCheckResponse.isFraudster()) {
+                throw new IllegalStateException("fraudster");
+            }
+
+            // todo: send notification
+            // todo: make it async. i.e add to queue
+            //notificationClient.sendNotification(
+            //        new NotificationRequest(
+            //                customer.getId(),
+            //                customer.getEmail(),
+            //                String.format("Hi %s, welcome to Amigoscode...",
+            //                        customer.getFirstName())
+            //        )
+            //);
+
+            NotificationRequest notificationRequest = new NotificationRequest(
+                    customer.getId(),
+                    customer.getEmail(),
+                    String.format("Hi %s, welcome to Amigoscode...",
+                            customer.getFirstName())
+            );
+            rabbitMQMessageProducer.publish(
+                    notificationRequest,
+                    "internal.exchange",
+                    "internal.notification.routing-key"
+            );
+        }
+
+
+}
