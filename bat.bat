e15f9e0b7af954b9d97e319704e4c4623cd7fdd2
Dario Fulci <DarioFulci@gmail.com>
2022-08-19 14:29:05 +0200

    deleted files not usefull

--- bat.bat
+++
@@ -1,7 +0,0 @@
-echo "# microservices" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/RimbaudSolaris/microservices.git
-git push -u origin main
